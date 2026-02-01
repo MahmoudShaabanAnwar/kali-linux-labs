957786acb83e27b26be23d18548bb980d6a8f9e1
Mahmoud Shaaban <mahmod.mahmod201450@gmail.com>
2026-02-01 22:49:11 +0200

    Update backup_script.sh

--- lab-backup-script/backup_script.sh
+++ lab-backup-script/backup_script.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
+
+# اسم اليوزر
+USER_NAME=$(whoami)
+echo "Hello $USER_NAME"
+
+# اسم الفايل الجديد
+FILE_NAME="${USER_NAME}1100.txt"
+touch "$FILE_NAME"
+echo "Created file: $FILE_NAME"
+
+# ضغط الفايل
+BACKUP_NAME="BACKUP_$(date +'%Y%m%d_%H%M%S').tar.gz"
+tar -czf "$BACKUP_NAME" "$FILE_NAME"
+
+echo "Backup created: $BACKUP_NAME"
 
